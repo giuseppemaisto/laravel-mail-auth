45dbf013fdac0b4295d2e432250592aa18674f18
giuseppemaisto <gmaisto98@gmail.com>
2023-03-08 16:35:57 +0100

    comandi in terminale e model creati

---
+++ resources/views/emails/new_post_added.blade.php
@@ -0,0 +1,7 @@
+<h1>Nuovo progetto inserito</h1>
+<p>nuovo progetto <br>
+    titolo:{{$lead->title}} <br>
+    slug:{{$lead->slug}} <br>
+    language:{{$lead->language}} <br>
+    titolo:{{$lead->title}} <br>
+</p>
